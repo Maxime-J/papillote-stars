45b027db17e80786b9aa9557aa92146a533eef66
Maxime-J <Maxime-J@users.noreply.github.com>
2024-12-26 11:48:14 +0100

    Update format

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Stars from './Stars';
 
-function papilloteBackground(){
+function papilloteBackground() {
   const root = createRoot(document.getElementById('back'));
 
-  return new Promise((res, rej) =>{
+  return new Promise((res, rej) => {
     root.render(
       <StrictMode>
         <Stars callback={res} />
